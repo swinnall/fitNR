ff2fd510c0914ae58c4cbc3a9bcb71231f377dcc
swinnall <samwinnall@icloud.com>
2022-06-21 15:44:33 +0200

    Update refnxTest.py

--- src/refnxTest.py
+++ src/refnxTest.py
@@ -11,135 +11,203 @@ from refnx.reflect import SLD, Slab, ReflectModel, Structure, LipidLeaflet
 from sldAnalysis import mainCalcSolvFrac
 
 
-print(f"refnx: {refnx.version.version}\n"
+def getFilePaths():
+    
+    file_paths = ['../input/S14_excel.txt','../input/S11_excel.txt']
+
+    expProperties = [{'solvType': 'd2o','monolayerType': 'h-MC3'},\
+                     {'solvType': 'acmw','monolayerType': 'd-MC3'}]
+    
+    return file_paths, expProperties
+
+
+
+def buildModel(expProperties):
+
+    # # parameter for the area per molecule each molecule occupies at the surface
+    # apm = Parameter(60, 'area per molecule', vary=True, bounds=(50, 100))
+     
+    
+    # # the sum of scattering lengths for the lipid head and tail in Angstrom
+    # if expProperties.get("monolayerType") == 'h-MC3':
+    #     b_heads = Parameter(18.8810e-4, 'b_heads')
+    #     b_tails = Parameter(-7.5180e-4, 'b_tails')
+        
+    # elif expProperties.get("monolayerType") == 'd-MC3':
+    #     b_heads = Parameter(18.8810e-4, 'b_heads')
+    #     b_tails = Parameter(637.9020e-4, 'b_tails')        
+
+    # # the volume occupied by the head and tail groups in cubic Angstrom
+    # v_heads = Parameter(260, 'v_heads')
+    # v_tails = Parameter(1030, 'v_tails')
+
+    # # check SLD
+    # #print(b_heads/v_heads)
+    # #print(b_tails/v_tails)
+
+    # # the head and tail group thicknesses.
+    # head_thickness = Parameter(6.5, 'head_thickness', vary=False, bounds=(5, 10))
+    # tail_thickness = Parameter(10.9, 'tail_thickness', vary=False, bounds=(10, 17))
+
+    # # define the head solvent fraction 
+    # head_solvent = Parameter(mainCalcSolvFrac(0,tail_thickness,head_thickness), 'head_solvent', vary=False, bounds=(0,1))
+    #   # in version 0.1.30 you can set constraints 
+    #   # head_solvent.set_constraint(mainCalcSolvFrac(0,tail_thickness,head_thickness))
+    # tail_solvent = Parameter(0, 'tail_solvent')
+
+    # # roughness is calculated for all layers 
+    # roughness = 3.5
+
+    # # tail groups face upwards
+    # leaflet = LipidLeaflet(apm,
+    #                              b_heads, v_heads, head_thickness,
+    #                              b_tails, v_tails, tail_thickness,
+    #                              roughness, roughness, 
+    #                              #head_solvent=(head_solvent), tail_solvent=(tail_solvent),
+    #                              # tail_solvent=(tail_solvent),
+    #                              reverse_monolayer=False)
+
+
+    ###### TEST ######
+    # roughness is calculated for all layers 
+    roughness = 3.5
+    
+    # solvent contrasts; vary `real` parameter with uniform bounds; `setp` parameter method changes many aspects at once
+    if expProperties.get("solvType") == 'd2o':
+        solvent = SLD(6.36 + 0j)
+        solvent.real.setp(vary=True, bounds=(6.1, 6.4))
+        solvent.real.name='d2o SLD'
+   
+    elif expProperties.get("solvType") == 'acmw':
+        solvent = SLD(0 + 0j)
+        solvent.real.setp(vary=True, bounds=(0, 0.02))
+        solvent.real.name='acmw SLD'
+    solvent.name = 'buffer'  
+    
+    # define tail as slab object
+    if expProperties.get("monolayerType") == 'h-MC3': 
+        tail = SLD(-0.0730 + 0j)
+        tail.real.setp(vary=False)
+        
+    elif expProperties.get("monolayerType") == 'd-MC3':
+        tail = SLD(6.1932 + 0j)
+        tail.real.setp(vary=False)
+
+    tail.name = 'tails'    
+    tailSlab = tail(10.9,roughness)
+    tailSlab.thick.setp(10.9,vary=False,bounds=(10,17))
+    tailSlab.thick.name = 'tails thickness'
+    tailSlab.rough.setp(roughness,vary=False)
+    tailSlab.rough.name = 'tails roughness'
+    tailSlab.vfsolv.setp(0,vary=False)
+    tailSlab.vfsolv.name = 'tails solvation fraction'
+
+    
+    head = SLD(0.7262 + 0j)
+    head.name = 'heads'
+    headSlab = head(6.5,roughness)
+    headSlab.thick.setp(6.5,vary=False,bounds=(5,10))
+    headSlab.thick.name = 'heads thickness'
+    headSlab.rough.setp(roughness,vary=False)
+    headSlab.rough.name = 'heads roughness'
+    headSlab.vfsolv.setp(mainCalcSolvFrac(0,tailSlab.thick,headSlab.thick),vary=False)
+    headSlab.vfsolv.name = 'heads solvation fraction'
+    
+    # make air slab 
+    air = SLD(0 + 0j)
+    air.name = 'air'
+
+    # make structure 
+    #s_ = air(0,roughness) | leaflet | solvent(0,roughness) # (10.9,roughness)
+    s_ = air(0,roughness) | tailSlab | headSlab | solvent(0,roughness)
+    
+    ### 
+    'Problem: only by creating models with common slabs within structures \
+        can you constrain parameters to one another. E.g. only solvent differing \
+        This cant be done as SL always differs so always need a new slab/leaflet\
+        Would need to constrain underpinning parameters... E.g. thickness \
+        Could couple hMC3 in D2O and ACMW but not to d-MC3 in the same contrasts'
+
+    # generate model data 
+    model_ = ReflectModel(s_)
+
+    model_.scale.setp(vary=False, bounds=(0.9, 1.1))
+    model_.bkg.setp(vary=True, bounds=(4.5e-7, 5.5e-7))
+    
+    return s_, model_
+
+
+
+
+def plotFit(global_objective, s_List):
+    
+    plotObjective = True
+    if plotObjective == True:
+        global_objective.plot()
+        plt.yscale('log')
+        plt.yscale('log')
+        plt.xlabel('Q / $\AA^{-1}$')
+        plt.ylabel('Reflectivity')
+        plt.legend()
+    
+    # plot SLD 
+    plotSLD = True 
+    if plotSLD == True:
+        fig, ax = plt.subplots()
+        for i in range(len(s_List)):
+            ax.plot(*s_List[i].sld_profile(), label='')
+        ax.set_ylabel("$\\rho$ / $10^{-6} \AA^{-2}$")
+        ax.set_xlabel("z / $\AA$")
+        ax.legend() 
+    
+    return 
+
+
+
+def main():
+    
+    # initialise variables 
+    objList = []; s_List = []
+    
+    # get path names of the NR experimental datasets 
+    file_paths, expProperties = getFilePaths()
+    
+    for idx, path in enumerate(file_paths):
+        
+        # convert to reflect dataset object
+        data_ = ReflectDataset(path) 
+       
+        # generate model 
+        s_, model_ = buildModel(expProperties[idx])
+        
+        # generate objective for given model (model, experiment)
+        objective_ = Objective(model_, data_)
+        
+        # append objective to list 
+        objList.append(objective_)
+        s_List.append(s_)
+
+    # combine all individual objectives into a GlobalObjective 
+    global_objective = GlobalObjective(objList)
+
+    # curve fitter object performs leaste squares fitting or MCMC sampling 
+    fitter = CurveFitter(global_objective, nwalkers=1e4)
+    
+    # do fit
+    fitter.fit('differential_evolution')#, constraints=(constraint,));
+    print(global_objective)
+    
+    # plot the output fits of all the samples 
+    plotFit(global_objective, s_List)
+    
+    return 
+
+
+
+if __name__ == '__main__':
+    print(f"refnx: {refnx.version.version}\n"
       f"scipy: {scipy.version.version}\n"
       f"numpy: {np.version.version}\n")
-
-# get test sample 
-file_path = '../input/S14_excel.txt'
-data_d2o = ReflectDataset(file_path)
-
-# solvent contrasts
-d2o   = SLD(6.36 + 0j)
-acmw  = SLD(0.00 + 0j)
-
-# vary `real` parameter with uniform bounds
-# `setp` parameter method changes many aspects at once
-d2o.real.setp(vary=True, bounds=(6.1, 6.4))
-d2o.real.name='d2o SLD'
-
-# Parameter for the area per molecule each DMPC molecule occupies at the surface. We
-# use the same area per molecule for the inner and outer leaflets.
-apm = Parameter(100, 'area per molecule', vary=False, bounds=(90, 110))
-
-# the sum of scattering lengths for the lipid head and tail in Angstrom
-b_heads = Parameter(18.8810e-4, 'b_heads')
-b_tails = Parameter(-7.5180e-4, 'b_tails')
-
-# the volume occupied by the head and tail groups in cubic Angstrom
-v_heads = Parameter(260, 'v_heads')
-v_tails = Parameter(1030, 'v_tails')
-
-# the head and tail group thicknesses.
-head_thickness = Parameter(6.5, 'head_thickness', vary=False, bounds=(5, 10))
-tail_thickness = Parameter(10.9, 'tail_thickness', vary=False, bounds=(10, 17))
-
-# define the head solvent fraction 
-head_solvent = Parameter(mainCalcSolvFrac(0,tail_thickness,head_thickness), 'head_solvent', vary=False, bounds=(0,1))
-# in version 0.1.30 you can set constraints 
-#head_solvent.set_constraint(mainCalcSolvFrac(0,tail_thickness,head_thickness))
-tail_solvent = Parameter(0, 'tail_solvent')
-
-# check SLD
-#print(b_heads/v_heads)
-#print(b_tails/v_tails)
-
-# roughness is calculated for all layers 
-roughness = 3.5
-
-# tail groups face upwards
-leaflet = LipidLeaflet(apm,
-                             b_heads, v_heads, head_thickness,
-                             b_tails, v_tails, tail_thickness,
-                             roughness, roughness, 
-                             head_solvent=(head_solvent), tail_solvent=(tail_solvent),
- #                            tail_solvent=(tail_solvent),
-                             reverse_monolayer=False)
-
-# make air slab 
-air = SLD(0 + 0j)
-
-# make structure 
-s_d2o = air(0,roughness) | leaflet | d2o(0,roughness)
-
-# generate model data 
-model_d2o = ReflectModel(s_d2o)
-
-model_d2o.scale.setp(vary=False, bounds=(0.9, 1.1))
-model_d2o.bkg.setp(vary=True, bounds=(4.5e-7, 5.5e-7))
-
-objective_d2o = Objective(model_d2o, data_d2o)
-
-# combine all individual objectives into a GlobalObjective 
-global_objective = GlobalObjective([objective_d2o])
-
-# curve fitter object performs leaste squares fitting or MCMC sampling 
-fitter = CurveFitter(global_objective, nwalkers=1e3)
-
-
-
-# from scipy.optimize import NonlinearConstraint
-
-# class DEC(object):
-#     def __init__(self, pars, objective):
-#         # we'll store the parameters and objective in this object
-#         # this will be necessary for pickling in the future
-#         self.pars = pars
-#         self.objective = objective
-
-#     def __call__(self, x):
-#         # we need to update the varying parameters in the objective first
-#         self.objective.setp(x)
-#         #print(self.pars[0],self.pars[1])
-#         # print(mainCalcSolvFrac(0,self.pars[0],self.pars[1]))
-        
-#         #print(pars)
-#         headSolvFrac = mainCalcSolvFrac(0,self.pars[0],self.pars[1])
-        
-#         if self.pars[2] == headSolvFrac:
-#             return self.pars[2]
-#         else: 
-#             return 2
-        
-        
-    
-#     ###return mainCalcSolvFrac(0,self.pars[0],self.pars[1])
-
-# pars = (s_d2o[1].thickness_tails, s_d2o[1].thickness_heads, s_d2o[1].head_solvent)
-# dec = DEC(pars, global_objective)
-
-# constraint = NonlinearConstraint(dec, 0, 1)
-
-# do fit and plot output 
-fitter.fit('differential_evolution')#, constraints=(constraint,));
-print(global_objective)
-
-
-
-
-# plot fit 
-global_objective.plot()
-plt.yscale('log')
-plt.yscale('log')
-plt.xlabel('Q / $\AA^{-1}$')
-plt.ylabel('Reflectivity')
-plt.legend()
-
-# plot SLD 
-fig, ax = plt.subplots()
-ax.plot(*s_d2o.sld_profile(), label='d2o')
-ax.set_ylabel("$\\rho$ / $10^{-6} \AA^{-2}$")
-ax.set_xlabel("z / $\AA$")
-ax.legend();
+    main()
 
 
